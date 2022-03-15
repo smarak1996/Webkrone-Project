c4d28d6da9b2dec37a5815cbc3bec5f3428ed97a
smarak1996 <smarakprd5@gmail.com>
2022-03-15 18:13:48 +0530

    third commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -38,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'taggit'
+    'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
